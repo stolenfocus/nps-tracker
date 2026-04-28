a07c1f7366198c289da7d91de43457b1291df1d2
bri <bri@briui-Macmini.local>
2026-04-28 19:01:11 +0900

    Daily feed update 2026-04-28

--- src/components/AdSlot.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function AdSlot({ label }: { label?: string }) {
-  return (
-    <div className="my-4 flex items-center justify-center min-h-[90px] bg-navy-light/50 border border-navy-lighter rounded text-slate-600 text-[10px]">
-      {label || "Advertisement"}
-    </div>
-  );
-}
